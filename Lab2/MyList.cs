01f890a47ec82846df92523c99d50cfdf922a86f
exponenta.euler <exponenta.euler@gmail.com>
2021-06-04 01:20:19 +0500

    продолжаю заводить реализацию списка.

--- Lab2/MyList.cs
+++ Lab2/MyList.cs
@@ -1,29 +1,86 @@
 ﻿using System.Collections;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace Lab2
 {
-    internal class MyList : Figure, IEnumerator, IEnumerable
+    internal class MyNode
     {
-        
-        public IEnumerator GetEnumerator()
+        private MyNode nextMyNode;
+        private MyNode elem;
+    }
+
+    internal class MyList : IEnumerator, IEnumerable
+    {
+        private Figure[] arr;
+        private int count;
+        private int index = -1;
+
+        public MyList(int capacity)
         {
-            throw new System.NotImplementedException();
+            arr = new Figure[capacity];
+            count = capacity;
         }
 
+        private bool IsInRange(int index)
+        {
+            if (index >= 0 && index < Count)
+                return true;
+
+            return false;
+        }
+
+        //TO DO: логика индексатора
+        private Figure this[int index]
+        {
+            get { }
+            set { }
+        }
+
+        public int Capacity
+        {
+            get => arr.Length;
+            set
+            {
+                //TO DO: логика мутатора.
+                if (arr.Length == 0)
+                {
+                }
+            }
+        }
+
+        public int Count { get; set; }
+
+
+        public void Add(Figure item)
+        {
+        }
+
+        public void Sort()
+        {
+        }
+
+        public void RemoveAt(int index)
+        {
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return this;
+        }
+
+        // перемещение на одну позицию вперед в контейнере элементов
         public bool MoveNext()
         {
             throw new System.NotImplementedException();
         }
 
+        // перемещение в начало контейнера  
         public void Reset()
         {
-            throw new System.NotImplementedException();
+            index = -1;
         }
 
+        // текущий элемент в контейнере
         public object? Current { get; }
-        public override string GetName()
-        {
-            throw new System.NotImplementedException();
-        }
     }
 }
