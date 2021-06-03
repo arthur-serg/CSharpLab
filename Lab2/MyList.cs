c5dbe84490a6c17a74ca46138ee8af8b9fb04d1f
exponenta.euler <exponenta.euler@gmail.com>
2021-06-03 18:07:00 +0500

    initial MyList implementation

---
+++ Lab2/MyList.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+
+namespace Lab2
+{
+    internal class MyList : Figure, IEnumerator, IEnumerable
+    {
+        
+        public IEnumerator GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool MoveNext()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public object? Current { get; }
+        public override string GetName()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
