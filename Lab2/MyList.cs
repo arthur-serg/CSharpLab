a60c4f4023932186be3e4bcc4c48382b8f155b46
exponenta.euler <exponenta.euler@gmail.com>
2021-06-21 20:35:37 +0500

    style fix

--- Lab2/MyList.cs
+++ Lab2/MyList.cs
@@ -108,13 +108,13 @@ namespace Lab2
 
         public IEnumerator<T> GetEnumerator()
         {
-            return ((IEnumerable<T>) myArray.Where(x=> !EqualityComparer<T>.Default.Equals(x, default(T)))).GetEnumerator();
+            return (myArray.Where(x => !EqualityComparer<T>.Default.Equals(x, default)))?.GetEnumerator();
         }
 
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return (IEnumerator)GetEnumerator();
+            return GetEnumerator();
         }
 
         // текущий элемент в контейнере
